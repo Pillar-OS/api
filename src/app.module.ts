1d22e3738d1ed52eb966f9adbd799cd570c0e8af
Grim's Desktop <paulperson.pro@gmail.com>
2022-09-23 21:24:02 +0200

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from './config/config.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+
+@Module({
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getMongoConfig(),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
